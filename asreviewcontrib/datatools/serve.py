27c40c3ea9de3945cdbdef3b7a22a6cab169627d
Laurens de Bruin <laurens.de.bruin99@gmail.com>
2023-11-09 08:03:54 +0100

    Alpha version of serve script
    untested initial code

--- asreviewcontrib/datatools/serve.py
+++ asreviewcontrib/datatools/serve.py
@@ -10,15 +10,57 @@ from asreview.data.base import load_data
 
 
 def serve(file, *annotators):
-    dataframe = pd.read_csv(file)
-    important_columns = config.COLUMN_DEFINITIONS['title'] + config.COLUMN_DEFINITIONS['abstract'] + config.COLUMN_DEFINITIONS['doi']
-    dataframe =  dataframe[dataframe.columns.intersection(important_columns)]
-    #check all columns that contain "include" per row
-    #if all of the columns for that row are empty:
-        #store row in new dataframe
-    #add annotator columns to new dataframe
-    #return new dataframe
+    print(file)
 
+    print(annotators)
+
+    dataframe = pd.read_csv(file)
+
+    label_columns = [col for col in df.columns if df[col].astype(str).str.contains('include').any()]
+
+    print(label_columns)
+
+    annotation_df = pd.Dataframe(columns=dataframe.columns)
+
+    for row in range(len(dataframe)):
+        #check if none of the columns that contain "include" have a label 0 or 1
+        if not dataframe[label_columns].isin([0, 1]):
+            unlabeled_row = dataframe.iloc[row]
+            annotation_df = annotation_df.append(unlabeled_row)
+    
+    print(annotation_df)
+
+    #drop all columns except title, abstract, doi, and MID
+    important_columns = config.COLUMN_DEFINITIONS['title'] + config.COLUMN_DEFINITIONS['abstract'] + config.COLUMN_DEFINITIONS['doi'] + ['MID']
+    annotation_df =  annotation_df[annotation_df.columns.intersection(important_columns)]
+
+    print(annotation_df)
+
+    #add annotator columns to annotation dataframe
+    for annotator in annotators:
+        #create copy of dataframe for each annotator
+        df = annotation_df.copy()
+
+        #add title abstract annotation columns
+        df[f'TI-AB_IC1_{annotator}'] = pd.np.nan
+        df[f'TI-AB_IC2_{annotator}'] = pd.np.nan
+        df[f'TI-AB_IC3_{annotator}'] = pd.np.nan
+        df[f'TI-AB_IC4_{annotator}'] = pd.np.nan
+        df[f'TI-AB_other_exlusion_reason_{annotator}'] = pd.np.nan
+        df[f'TI-AB_final_label_{annotator}'] = pd.np.nan
+
+        #add full text annotation columns
+        df[f'FT_IC1_{annotator}'] = pd.np.nan
+        df[f'FT_IC2_{annotator}'] = pd.np.nan
+        df[f'FT_IC3_{annotator}'] = pd.np.nan
+        df[f'FT_IC4_{annotator}'] = pd.np.nan
+        df[f'FT_other_exlusion_reason_{annotator}'] = pd.np.nan
+        df[f'FT_final_label_{annotator}'] = pd.np.nan
+
+        #output new annotation dataframe
+        df.to_csv(annotator+".csv")
+
+    
 
 def main():
     serve(sys.argv[1:])
